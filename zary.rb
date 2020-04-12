d4fbe446ff68d39a766de8e16b2b3fc29c3b54ce
Ushitora Anqou <ushitora@anqou.net>
2020-04-12 13:09:52 +0900

    Add DummyClient and MastoClient

--- zary.rb
+++ zary.rb
@@ -1,26 +1,61 @@
 require "mastodon"
 require "sanitize"
+require "ostruct"
+require "pp"
 
-MASTODON_HOST = "https://mstdn.anqou.net"
+class MastoClient
+  def initialize(host, access_token)
+    @rest = Mastodon::REST::Client.new(base_url: host, bearer_token: access_token)
+    @stream = Mastodon::Streaming::Client.new(base_url: host, bearer_token: access_token)
+  end
 
-raise "set MASTODON_ACCESS_TOKEN" unless ENV.key? "MASTODON_ACCESS_TOKEN"
+  def stream
+    @stream.user do |event|
+      next unless event.is_a?(Mastodon::Status)
+      next unless event.attributes["reblog"].nil?
+      next if event.account.bot?
+      next unless event.visibility == "public" or event.visibility == "unlisted"
+      next unless event.account.url =~ /mstdn\.anqou\.net/
 
-rest = Mastodon::REST::Client.new(base_url: MASTODON_HOST, bearer_token: ENV["MASTODON_ACCESS_TOKEN"])
-stream = Mastodon::Streaming::Client.new(base_url: MASTODON_HOST, bearer_token: ENV["MASTODON_ACCESS_TOKEN"])
-stream.user do |event|
-  #pp event
+      yield event, @rest
+    end
+  end
+end
 
-  next unless event.is_a?(Mastodon::Status)
-  next unless event.attributes["reblog"].nil?
-  next if event.account.bot?
-  next unless event.visibility == "public" or event.visibility == "unlisted"
-  next unless event.account.url =~ /mstdn\.anqou\.net/
+class DummyClient
+  class DummyRest
+    def create_status(msg, options)
+      pp(["out", { msg: msg, options: options }])
+    end
+  end
 
+  def stream
+    rest = DummyRest.new
+    while gets
+      event = OpenStruct.new({
+        id: 100,
+        account: OpenStruct.new({ acct: "dummyuser0" }),
+        content: $_,
+      })
+      pp(["in", event])
+      yield event, rest
+    end
+  end
+end
+
+client = if ENV.key? "MASTODON_ACCESS_TOKEN"
+    puts "Use MastoClient..."
+    MastoClient.new("https://mstdn.anqou.net", ENV["MASTODON_ACCESS_TOKEN"])
+  else
+    puts "Use DummyClient..."
+    DummyClient.new
+  end
+
+client.stream do |event, rest|
   content = Sanitize.fragment(event.content)
 
-  if content =~ /わいわい/
-    puts "===== WAIWAI ====="
-    p event
+  case content
+  when /わいわい/
     rest.create_status("@#{event.account.acct} :bar_ter__: ＜わいわいするなわくわくしろ", in_reply_to_id: event.id)
   end
 end
